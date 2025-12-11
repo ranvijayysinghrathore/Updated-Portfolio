c3214e6e9591f0ee6237c34618c901def2923ee0
ChanchalSinghRathore <chanchalrathore111003@gmail.com>
2025-12-11 16:02:25 +0530

    feat: add FloatingDock component and demo, update dependencies, and include new assets

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,4 +1,4 @@
-import { link } from "framer-motion/client"
+// import { link } from "framer-motion/client"
 import Work1 from "../../assets/work1.png"
 import Work2 from "../../assets/work2.jpg"
 import Work3 from "../../assets/work3.jpeg"
@@ -9,6 +9,7 @@ import Work7 from "../../assets/Work7.jpeg"
 import Work8 from "../../assets/Work8.png"
 import Work9 from "../../assets/Work9.jpeg"
 import Work10 from "../../assets/Work10.png"
+import Work11 from "../../assets/Work11.png"
 
 
 export const projectsData = [
@@ -29,6 +30,16 @@ export const projectsData = [
     category: "web",
     link:"https://anonymous-messenger-ten.vercel.app/"
   },
+
+  
+    {
+    id: 11,
+    image: Work11,
+    title: "Future Note",
+    category: "web",
+    link:"https://futurenote4u.vercel.app/"
+  },
+
 
    {
       id: 5,
