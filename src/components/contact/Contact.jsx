9321d2b1a0ad791221c92503145a924a0d560727
ChanchalSinghRathore <chanchalrathore111003@gmail.com>
2026-02-15 02:01:58 +0530

    idk2

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -40,8 +40,8 @@ const Contact = () => {
                             <i className="bx bx-mail-send contact__card-icon"></i>
 
                             <h3 className="contact__card-title">Email</h3>
-                            <span className="contact__card-data">chanchal151003@gmail.com</span>
-                            <a href="mailto:chanchal151003@gmail.com" className="contact__button">Write me  <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
+                            <span className="contact__card-data">ranvijayyrathore@gmail.com</span>
+                            <a href="mailto:ranvijayyrathore@gmail.com" className="contact__button">Write me  <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
                         </div>
 
                         <div className="contact__card">
