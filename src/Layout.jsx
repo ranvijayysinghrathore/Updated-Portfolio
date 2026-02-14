d969d3dc19c6ac4bc23e07f0c4cd868e201ec2c3
ChanchalSinghRathore <chanchalrathore111003@gmail.com>
2026-02-14 16:53:58 +0530

    feat: Introduce base layout component and social media links.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Footer from './components/footer/Footer';
 import { Outlet } from 'react-router-dom'
-import About from './components/about/About';
-import InfoSection from './InfoSection';
-import { useEffect, useState } from "react";
 import CursorEffect from './components/cursor/CursorEffect';
 import { Analytics } from "@vercel/analytics/react"
 
